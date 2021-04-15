0f4146434c6ea38eda8374adb1f64105467f398f
brandnn07 <brandcarter4@gmail.com>
2021-04-15 17:54:42 -0400

    added components and finished appjs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from './components/Header';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Projects from './components/Projects';
+import AboutMe from './components/AboutMe';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/Contact">
+          <Contact />
+        </Route>
+        <Route exact path="/Footer">
+          <Footer />
+        </Route>
+        <Route exact path="/Projects">
+          <Projects />
+        </Route>
+        <Route exact path="/AboutMe">
+          <AboutMe />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
